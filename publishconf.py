61b1c12d89a5c705bfcad3462f4c4a67eff29d21
jczetta <jczetta@gmail.com>
2015-06-28 20:26:11 -0400

    Switch to org url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://coopcomics.github.io'
+SITEURL = 'http://co-op-comics.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
